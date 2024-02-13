9e12b38cd5006d3a0b8e48d9592d945b620bce43
ironsubhajit <mandalsubhajit655@gmail.com>
2024-02-14 02:47:42 +0530

    responsiveness added to domain containers

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
 import { Project } from 'src/app/interfaces/project';
 import { ProjectService } from 'src/app/services/project/project.service';
 
@@ -10,12 +10,41 @@ import { ProjectService } from 'src/app/services/project/project.service';
 export class HomePageComponent {
   projectList: Project[] = [];
 
+  screenWidth!: number;
+  customHeight!: { [key: string]: string };
+
   imagePath: string = '/assets/vector_images/welcome_banner.png'
 
-  constructor(private projectService: ProjectService) {}
+  constructor(private projectService: ProjectService, private cdr: ChangeDetectorRef) {
+     // Initialize the screenWidth property with the initial window height
+     this.screenWidth = window.innerWidth;
+     this.customHeight = { height: '26.25rem' };
+  }
 
   ngOnInit(): void {
     this.getProjectList();
+    this.adjustWidth();
+  }
+
+  // Listen for window resize events
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    // Update the screenWidth property with the new window width
+    this.screenWidth = event.target.innerHeight;
+    // Call a method to adjust the height based on the screen size
+    this.adjustWidth();
+    this.cdr.detectChanges(); // Trigger change detection
+  }
+
+  // Method to adjust the height based on the screen size
+  adjustWidth() {
+    // console.log("width ==> ", this.screenWidth)
+    if (this.screenWidth <= 767) {
+      this.customHeight = { height: '25.5rem' };
+    } else {
+      // Set the height value for larger screens
+      this.customHeight = { height: '26.25rem' };
+    }
   }
 
   getProjectList(): void {
